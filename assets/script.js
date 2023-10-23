ca270ff5ef537ad797044f97a83521ec915c5946
Emily Rosenberg <earosenberg@gmail.com>
2023-10-23 13:30:23 +0200

    Add JavaScript structure

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+function runGame() {
+
+}
+
+let questions = []
+
+// function fetch(questions.json).then(res => {
+//     console.log(res);
+//     return res.json();
+// })
+// .then(loadedQuestions => {
+//     console.log(loadedQuestions);
+//     questions = loadedQuestions;
+// });
+
+function displayQuestion() {
+    // document.getElementsByClassName("question").textContent = 
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
