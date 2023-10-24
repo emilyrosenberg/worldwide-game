30ff7b346587ee0d001f46e713c75a7c0a9596e7
Emily Rosenberg <earosenberg@gmail.com>
2023-10-24 15:55:05 +0200

    Add comments to clarify functions

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,41 @@
 // From Love Maths walkthrough project
+document.addEventListener("DOMContentLoaded"), function() {
+    let button = document.getElementsByTagName("button");
+}
+
+// Idea from https://www.sitepoint.com/simple-javascript-quiz/
+// Array of quiz questions generated from Open Trivia Database https://opentdb.com/api_config.php
+let quizQuestions = [
+    {question: "The Republic of Malta is the smallest microstate worldwide.",
+    correctAnswer: "false"
+    }
+     // more questions go here
+]
+
+// From tutoring (Roman)
+// Display question from question array above
+// Then show the next question
+function displayQuestion() {
+    document.getElementById('question').textContent = quizQuestions[0].question;
+    }
+    displayQuestion();
+// Add code to display the next question
+
+//  Listen for true/false button click event
+ button.addEventListener("click", function() {
+        checkAnswer();
+    })
+
+// If true button is clicked and answer = true, show alert and increment score and show the next question. If answer true button is clicked and answer = false, show alert and show the next question. And vice-versa.
+function checkAnswer() {
+
+}
+
+// If checkAnswer returned correct then increment the score
+function incrementScore() {
+
+}
+
 // document.addEventListener("DOMContentLoaded", function() {
 //     let buttons = document.getElementsByTagName("button");
 
@@ -12,24 +49,6 @@
 //             }
 //         });
 //     }    
-
-
-// Idea from https://www.sitepoint.com/simple-javascript-quiz/
-
-let quizQuestions = [
-    {question: "The Republic of Malta is the smallest microstate worldwide.",
-    correctAnswer: "false"
-    }
-     // more questions go here
-]
-
-// From tutoring (Roman)
-function displayQuestion() {
-    document.getElementById('question').textContent = quizQuestions[0].question;
-    }
-    displayQuestion();
-
-   
 
 // function displayQuestion(quizQuestions) {
 //     document.getElementById('question').textContent = quizQuestions;
@@ -48,16 +67,3 @@ function displayQuestion() {
 //     console.log(loadedQuestions);
 //     questions = loadedQuestions;
 // });
-
-function runGame() {
-
-}
-
-function checkAnswer() {
-
-}
-
-function incrementScore() {
-
-}
-
