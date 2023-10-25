dbe72dfb1f44b43bdccf7a2a78e315f5a7c6a96b
Emily Rosenberg <earosenberg@gmail.com>
2023-10-25 12:33:02 +0200

    Add code to show the total number of questions in the score box

--- assets/script.js
+++ assets/script.js
@@ -1,88 +1,83 @@
 // Idea from "How to create a TRUE or FALSE quiz question using JavaScript" https://www.youtube.com/watch?v=MnkD82iHe30
 let currentQuestion = 0;
-let questions = [
-    {"question": "The Republic of Malta is the smallest microstate worldwide.",
+let quizQuestions = [
+    {question: "The Republic of Malta is the smallest microstate worldwide.",
+    answer: false,
+    },
+    {question: "Greenland is almost as big as Africa.",
+    answer: false,
+    },
+    {question: "Greenland is covered with grass and Iceland covered with ice.",
+    answer: false,
+    },
+    {question: "There is a city called Rome in every continent on Earth.",
     "answer": false,
     },
-    {"question":"Greenland is almost as big as Africa.",
-    "answer": false,
-    },
-    {"question": "Greenland is covered with grass and Iceland covered with ice.",
-    "answer": false,
-    },
-    {"question": "There is a city called Rome in every continent on Earth.",
-    "answer": false,
-    },
-    {"question": "California is larger than Japan.",
-    "answer": true,
+    {question: "California is larger than Japan.",
+    answer: true,
     }
 ];
 
 (function() {
-    fnStart();
+    startGame();
 });
 
-document.getElementById("correct-scores").innerHTML = 0;
+document.getElementById("correct-scores").innerText = 0;
 
-document.getElementById("next-question").addEventListener("click", function(event) {
-    fnCheck();
-});
+document.getElementById("quiz-questions-length").innerText = quizQuestions.length;
 
 document.getElementById("true-button").addEventListener("click", function(event) {
-    fnCheck(true);
+    checkAnswer(true);
 });
 
 document.getElementById("false-button").addEventListener("click", function(event) {
-    fnCheck(false);
+    checkAnswer(false);
 });
 
 document.getElementById("next-question").addEventListener("click", function(event) {
-    fnNext();
+    nextQuestion();
 });
 
-function fnStart () {
-    document.getElementById("question-text").innerHTML = questions[currentQuestion].question;
+// FIX THIS: Loads the question text for the first question
+function startGame () {
+    document.getElementById("question-text").innerText = quizQuestions[currentQuestion].question;
     currentQuestion = 0;
-    // document.getElementById("next-question").classList.add("hide"); 
+    // document.getElementById("quiz-questions-length").innerText = quizQuestions.length;
 }
 
-function fnCheck(answer) {
-    let userAnswer = questions[currentQuestion].answer;
+// Checks whether the user answer matches the real answer, shows an alert, and triggers the function for incrementing the score
+function checkAnswer(answer) {
+    let userAnswer = quizQuestions[currentQuestion].answer;
     if (userAnswer === answer) {
         alert("Great job!");
         incrementScore();
     } else {
         alert("Oops, try again!");
-        // Add code to prevent this when "Next answer button is clicked"
     }
 }
 
-/**
- * Gets the current score from the DOM and increments it by 1
- */
+// Gets the current score from the DOM and increments it by 1
 function incrementScore() {
-    if (currentQuestion <= questions.length) {
+    if (currentQuestion <= quizQuestions.length) {
         let oldScore = parseInt(document.getElementById("correct-scores").innerText);
         document.getElementById("correct-scores").innerText = ++oldScore;
     } else {
-        // Add code to stop counting up?
-        document.getElementById("correct-scores").innerText = questions.length;
+        // FIX THIS: Add code to stop counting up
+        document.getElementById("correct-scores").innerText = quizQuestions.length;
     }
 }
 
-function fnNext() {
-    // document.getElementById(question-text).classList.add("hide");
-    if(currentQuestion < questions.length - 1) {
+// Shows the next question when the button is clicked
+function nextQuestion() {
+    if(currentQuestion < quizQuestions.length - 1) {
         currentQuestion = currentQuestion + 1;
-    //     document.getElementById("next-question").classList.add("hide");
-    // } else {
-    //     document.getElementById("next-question").classList.add("hide");
-    // }
-    document.getElementById("question-text").innerHTML = questions[currentQuestion].question;
+    }
+    {document.getElementById("question-text").innerHTML = quizQuestions[currentQuestion].question;
 }}
 
 
 
+// OLD IDEAS:
 // Idea from https://www.sitepoint.com/simple-javascript-quiz/
 // Array of quiz questions generated from Open Trivia Database https://opentdb.com/api_config.php
 // let quizQuestions = [
@@ -108,7 +103,7 @@ function fnNext() {
 // From tutoring (Roman)
 // Display question from question array above
 // function displayQuestion() {
-//     document.getElementById('question').textContent = quizQuestions[0].question;
+//     document.getElementById('question-text').textContent = quizQuestions[0].question;
 //     }
 //     displayQuestion();
 // Add code to display the next question
