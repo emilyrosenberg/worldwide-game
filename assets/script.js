c8c789108ddb68b8f5d216d923534213c9f4e8d7
Emily Rosenberg <earosenberg@gmail.com>
2023-10-26 15:20:48 +0200

    Remove commented-out code and update readme

--- assets/script.js
+++ assets/script.js
@@ -18,10 +18,6 @@ let quizQuestions = [
     }
 ];
 
-// (function() {
-//     startGame();
-// });
-
 // Hides user feedback elements
 document.getElementById("response-correct").classList.add("hide");
 document.getElementById("response-incorrect").classList.add("hide");
@@ -53,11 +49,9 @@ document.getElementById("play-again-button").addEventListener("click", function(
 });
 
 let answered = false;
-// let finalScore = document.getElementById("score-area");
 
 function checkAnswer(answer) {
     let userAnswer = quizQuestions[currentQuestion].answer;
-    
     // Don't process if the user has already answered
     // Ideas for setting up this flag and disabling the buttons are from tutoring (Martin)
     if (answered) {
@@ -65,11 +59,9 @@ function checkAnswer(answer) {
     }
 
     if (userAnswer === answer) {
-        // alert("Great job!");
         document.getElementById("response-correct").classList.remove("hide");
         incrementScore();
     } else {
-        // alert("Oops, that's not the right answer. But try the next question!");
         document.getElementById("response-incorrect").classList.remove("hide");
     }
     document.getElementById("true-button").disabled = true;
@@ -90,101 +82,21 @@ function incrementScore() {
 function nextQuestion() {
     document.getElementById("response-correct").classList.add("hide");
     document.getElementById("response-incorrect").classList.add("hide");
-    // Gets the final score to show at the end
-    // document.getElementById("final-score-number").innerText = finalScore;
     if (currentQuestion < quizQuestions.length - 1) {
         currentQuestion = currentQuestion + 1;
     } else {
         document.getElementById("next-question").classList.add("hide");
         document.getElementById("play-again-button").classList.remove("hide");
     }
-    // Enable the answer buttons for the next question
+    // Enables the answer buttons for the next question
     document.getElementById("true-button").disabled = false;
     document.getElementById("false-button").disabled = false;
     document.getElementById("question-text").innerHTML = quizQuestions[currentQuestion].question;
-    // Reset the answered flag
+    // Resets the answered flag
     answered = false; 
     document.getElementById("question-text").innerHTML = quizQuestions[currentQuestion].question;
 }
 
 function resetGame() {
-    location.reload()
+    location.reload();
 }
-
-// OLD IDEAS:
-// Idea from https://www.sitepoint.com/simple-javascript-quiz/
-// Array of quiz questions generated from Open Trivia Database https://opentdb.com/api_config.php
-// let quizQuestions = [
-//     {question: "The Republic of Malta is the smallest microstate worldwide.",
-//     answer: "false",
-//     }
-
-// ]
-    // future questions
-    // {question: "Greenland is almost as big as Africa.",
-    // answer: "false"
-    // },
-    // {question: "Greenland is covered with grass and Iceland covered with ice.",
-    // answer: "false",
-    // },
-    // {question: "There is a city called Rome in every continent on Earth.",
-    // answer: "false",
-    // },
-    // {question: "California is larger than Japan.",
-    // answer: "true",
-    // }
-   
-// From tutoring (Roman)
-// Display question from question array above
-// function displayQuestion() {
-//     document.getElementById('question-text').textContent = quizQuestions[0].question;
-//     }
-//     displayQuestion();
-// Add code to display the next question
-
-//  Listen for true/false button click event
-//  button.addEventListener("click", function() {
-//         checkAnswer();
-//     })
-
-// If true button is clicked and answer = true, show alert and increment score and show the next question. If answer true button is clicked and answer = false, show alert and show the next question. And vice-versa.
-// function checkAnswer() {
-//     let isCorrect = userAnswer === correctAnswer[0];
-// }
-
-// If checkAnswer returned correct then increment the score
-// function incrementScore() {
-
-// }
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     let buttons = document.getElementsByTagName("button");
-
-//     for (let button of buttons) {
-//         button.addEventListener("click", function() {
-//             if (this.getAttribute("data-type") === "submit") {
-//                 checkAnswer();
-//             } else {
-//                 let gameType = this.getAttribute("data-type");
-//                 runGame(gameType);
-//             }
-//         });
-//     }    
-
-// function displayQuestion(quizQuestions) {
-//     document.getElementById('question').textContent = quizQuestions;
-// }
-
-// quizQuestions.forEach(
-//     (currentQuestion, questionNumber) => {
-//     }
-// )
-
-// function fetch(questions.json).then(res => {
-//     console.log(res);
-//     return res.json();
-// })
-// .then(loadedQuestions => {
-//     console.log(loadedQuestions);
-//     questions = loadedQuestions;
-// });
